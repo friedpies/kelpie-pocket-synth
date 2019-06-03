131bb9cff9f9dd01aaa2c2a54b14e51ba27473ba
Kenneth Marut <kenmarut@gmail.com>
2019-06-03 08:02:23 -0500

    Boilerplate setup
    Initialized repo with simple "Hello World" program, tested MK20 on Kelpie board, working

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <Audio.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("KELPIE ON PLATFORMIO");
+  // put your main code here, to run repeatedly:
+}
