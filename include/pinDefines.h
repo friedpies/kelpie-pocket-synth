d65f04036469874936b21d825f40ba3c9de6c9e1
Kenneth Marut <kenmarut@gmail.com>
2019-06-18 22:16:26 -0500

    test pins and write pin defines

---
+++ include/pinDefines.h
@@ -0,0 +1,24 @@
+#define KNOB1 20
+#define KNOB2 21
+#define KNOB3 31
+#define KNOB4 26
+#define KNOB5 A11
+#define KNOB6 29
+#define KNOB7 27
+#define KNOB8 28
+#define KNOB9 15
+#define KNOB10 30
+#define KNOB11 17
+#define KNOB12 16
+#define KNOB13 A14
+#define KNOB14 A10
+#define KNOB15 A12
+#define KNOB16 A13
+
+#define SWLED1 24
+#define SWLED2 25
+#define SWLED3 32
+#define SWLED4 33
+
+#define MIDILED 8
+// TO DO
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,48 @@
 #include <Arduino.h>
+// #include <Audio.h>
+
 #include <Audio.h>
+#include <Wire.h>
+#include <SD.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+#include <pinDefines.h>
 
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
   Serial.begin(9600);
+  pinMode(SWLED1, OUTPUT);
+  pinMode(SWLED2, OUTPUT);
+  pinMode(SWLED3, OUTPUT);
+  pinMode(SWLED4, OUTPUT);
+
+  digitalWrite(SWLED1, LOW);
+  digitalWrite(SWLED2, LOW);
+  digitalWrite(SWLED3, LOW);
+  digitalWrite(SWLED4, LOW);
+  digitalWrite(MIDILED, LOW);
 }
 
-void loop() {
-  Serial.println("KELPIE ON PLATFORMIO");
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(SWLED1, HIGH);
+  delay(150);
+  digitalWrite(SWLED1, LOW);
+  delay(150);
+
+  digitalWrite(SWLED2, HIGH);
+  delay(150);
+  digitalWrite(SWLED2, LOW);
+  delay(150);
+
+  digitalWrite(SWLED3, HIGH);
+  delay(150);
+  digitalWrite(SWLED3, LOW);
+  delay(150);
+
+  digitalWrite(SWLED4, HIGH);
+  delay(150);
+  digitalWrite(SWLED4, LOW);
+  delay(150);
+
 }
