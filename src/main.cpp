0c2785dc18112bf1f8315dbaf9b5c245c322dd89
Kenneth Marut <kenmarut@gmail.com>
2019-06-19 22:49:56 -0500

    tested midi working

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // #include <Audio.h>
 
 #include <Audio.h>
+#include <MIDI.h>
 #include <Wire.h>
 #include <SD.h>
 #include <SPI.h>
@@ -10,16 +11,22 @@
 #include <globalVariables.h>
 #include <Kelpie.h>
 
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+
 Kelpie kelpie(false); // true enables serial
 
 void setup()
 {
+  MIDI.begin();
 }
 
 void loop()
 {
-  kelpie.activateLights();
-  delay(500);
-  kelpie.deactivateLights();
-  delay(500);
+  while (MIDI.read()) {
+    Serial.println("MIDI NOTE RECIEVED");
+  }
+  // kelpie.activateLights();
+  // delay(500);
+  // kelpie.deactivateLights();
+  // delay(500);
 }
