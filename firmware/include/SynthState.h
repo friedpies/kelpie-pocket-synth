6da5746497b48b5ed2b0ddcb9732c722aeae7413
Kenneth Marut <kenmarut@gmail.com>
2019-11-13 17:32:57 -0600

    Fixed envelope issue on release (#54)
    * adding synthstate class
    
    * uploading graphics
    
    * working on new class, fixed buffer bug

---
+++ firmware/include/SynthState.h
@@ -0,0 +1,14 @@
+#ifndef SynthState_h
+#define SynthState_h
+
+#include <Arduino.h>
+#include <globalSynthState.h> // this is the state data type
+// #include <voices.h> // this is the voice data type
+
+class SynthState
+{
+  public:
+  SynthState(synthState state);
+};
+
+#endif
